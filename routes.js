a792df186038972a3f12d3415bb3b91cc0d0d9d6
Glitch (api-test-fun) <none>
2020-02-27 22:55:43 +0000

    🌖🐄 Checkpoint ./routes.js:3349592/1711

--- routes.js
+++ routes.js
@@ -70,51 +70,6 @@ var routes = function(app) {
     }
   });
 
-  //learn
-  app.get("/info", function(req, res) {
-    if (req.query.id)
-      return res.send({
-        message:
-          "You sent a query string parameter! It indicates you want some data associated with an id of 1.",
-        next:
-          "Now try adding a path parameter. Enter /:category before /info in the address. "+
-          "In Params, enter a value for the category row (e.g. 'hats' and click Send again."
-      });
-    else
-      return res.send({
-        message: "You sent a request!!!",
-        next:
-          "Now try a parameter  - add ?id=1 to the end of the address after /info and click Send again."
-      });
-  });
-  app.get("/:category/info", function(req, res){
-    
-      return res.send({
-        message: "You sent a path parameter!",
-        next:
-          "Now try changing the method - currently GET before the addresss - change it to POST and click Send again."
-      });
-  });
-  app.post("/:category/info", function(req, res) {
-    if (req.body.data)
-      return res.send({ message: "You sent body data!", next: "Now try..." });
-    else
-      return res.send({
-        message:
-          "You sent a post request! Post requests let you pass data to the API.",
-        next:
-          "Now try adding some data. " +
-          "Open the Body tab under the address, select Raw, and enter "+
-          "{'data':'hello'} into the empty pane, " +
-          "making sure JSON is selected from the dropdown."
-      });
-  });
-  app.get("/lesson", function(req, res) {
-    let responseData = new Object();     
-    responseData["message"] = "Click Visualize to see more info!";
-    responseData["information"] = "You sent a GET request...";
-    return res.send(responseData);
-  });
 };
 
 module.exports = routes;
