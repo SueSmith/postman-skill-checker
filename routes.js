3137dcd267d24c67d119824fe25b711cdf0020f6
Glitch (free-inexpensive-find) <none>
2020-08-03 17:57:07 +0000

    👴⛹ Checkpoint ./routes.js:3349592/5356

--- routes.js
+++ routes.js
@@ -251,15 +251,114 @@ var routes = function(app) {
   });
 
   //update user
-  app.patch("/customer", function(req, res) {
+  app.put("/customer", function(req, res) {
     const apiSecret = req.get("auth_key");
     if (!apiSecret)
-      res.status(401).json({ error: "You need to supply an auth key!" });
-    if (!req.body.name)
-      return res.send({ status: "error", message: "no name" });
-    else if (!req.body.type)
-      return res.send({ status: "error", message: "no type" });
-    else res.status(201).json({ status: "customer updated" });
+      res.status(401).json({
+        welcome:
+          "You're learning APIs 101! Check out the 'data' object below to see the values returned by the API. Click Visualize for a more " +
+          "readable view of the response.",
+        tutorial: {
+          title: "Your request is unauthorized! 🚫",
+          intro: "This endpoint requires authentication.",
+          steps: [
+            {
+              note:
+                "In **Auth** select **API Key** from the drop-down, enter `auth_key` as the **Key** and any text you like as the **Value**. " +
+                "Make sure you are adding to the **Header**."
+            }
+          ],
+          next: [
+            {
+              step: "With your auth key in place, click **Send** again."
+            }
+          ]
+        }
+      });
+    else if (!req.query.id)
+      res.status(400).json({
+        welcome:
+          "You're learning APIs 101! Check out the 'data' object below to see the values returned by the API. Click Visualize for a more " +
+          "readable view of the response.",
+        tutorial: {
+          title: "Your request is incomplete! ✋",
+          intro:
+            "This endpoint requires an ID representing the customer to update.",
+          steps: [
+            {
+              note:
+                "Include a path parameter by adding `/:customer_id` to the end of the request address. Your parameter will appear in **Params**. "+
+                "For the value copy the `id` of a customer you added when you sent the "+
+                  "`POST` request—copy it from the response in the `Get all customers` request. ***You can only update a customer you added.***"
+            }
+          ],
+          next: [
+            {
+              step: "With your customer ID parameter in place, click **Send** again."
+            }
+          ]
+        }
+      });
+    else if (!req.body.name || !req.body.type)
+      res.status(400).json({
+        welcome:
+          "You're learning APIs 101! Check out the 'data' object below to see the values returned by the API. Click Visualize for a more " +
+          "readable view of the response.",
+        tutorial: {
+          title: "Your request is incomplete! ✋",
+          intro:
+            "This endpoint requires body data representing the new customer.",
+          steps: [
+            {
+              note:
+                "In **Body** select **raw** and choose **JSON** instead of `Text` in the drop-down list. Enter the following JSON data " +
+                "including the enclosing curly braces:",
+              raw_data: {
+                name: "Dorothy Zpornak",
+                type: "Individual"
+              }
+            }
+          ],
+          next: [
+            {
+              step: "With your body data in place, click **Send** again."
+            }
+          ]
+        }
+      });
+    else {
+      var adminId = req.get("user-id") ? req.get("user-id") : "anonymous";
+      db.get("customers")
+        .push({
+          id: shortid.generate(),
+          name: req.body.name,
+          type: req.body.type,
+          admin: adminId
+        })
+        .write();
+      res.status(201).json({
+      welcome:
+        "You're learning APIs 101! Check out the 'data' object below to see the values returned by the API. Click Visualize for a more " +
+          "readable view of the response.",
+      tutorial: {
+        title: "You added a new customer! 🏅",
+        intro:
+          "Your new customer was added to the database.",
+        steps: [
+          {
+            note: "Go back into the first request you opened `Get all customers` and **Send** it again before returning here—"+
+              "you should see your new addition in the array!"
+          }
+        ],
+        next: [
+          {
+            step:
+              "Next open the `PUT Update customer` request and click **Send**."
+          }
+        ]
+      }
+    });
+    }
   });
 
   //protect everything after this by checking for the secret
