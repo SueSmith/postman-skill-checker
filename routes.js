f1dd88ebbccd58a9c5aeeab74cc38d98669a205b
Glitch (free-inexpensive-find) <none>
2020-08-23 12:35:18 +0000

    📷🌛 Checkpoint ./routes.js:3349592/13

--- routes.js
+++ routes.js
@@ -183,8 +183,8 @@ var routes = function(app) {
           {
             name: "Added a script",
             hint:
-              "Add script code to the request Tests to set a collection variable named 'responseData', with a value from the `rand` field in the response JSON. " +
-              "Hint: you'll need to run the request twice because the test code won't run until after the response is received.",
+              "Add script code to the request Tests to set a variable named 'responseData', with a value from the `rand` field in the response JSON. " +
+              "Hint: You'll need to run the request twice because the test code won't run until after the response is received.",
             value: learner.script > 0 ? true : false
           }
         ],
